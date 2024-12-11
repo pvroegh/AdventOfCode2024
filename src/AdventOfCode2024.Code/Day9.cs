fc2f7c16b8fe3eba8beffb9b888bfb64df482ab3
Patrick Vroegh <patrick.vroegh@acture.nl>
2024-12-11 19:10:23 +0100

    Day 9 part 2 WIP

--- src/AdventOfCode2024.Code/Day9.cs
+++ src/AdventOfCode2024.Code/Day9.cs
@@ -8,11 +8,11 @@ public class Day9 : BaseDay
     private readonly int _width;
     private readonly int _height;
 
-    
+
 
     public Day9(string? filename = null) : base(filename)
     {
-        }
+    }
 
     public override string Solution1()
     {
@@ -21,10 +21,7 @@ public class Day9 : BaseDay
             .Select((c, index) => new { Size = int.Parse(c.ToString()), FileId = index })
             .Select(x => Enumerable.Range(0, x.Size).Select(i => x.FileId))
             .SelectMany(x => x).ToArray();
-        var fileBlocksReversed = fileBlocks.Reverse().ToArray();
         var freeBlocks = Input[0].Where((_, index) => index % 2 != 0).Select(c => int.Parse(c.ToString())).ToArray();
-
-        var compressedSpace = new List<int>();
 
         long sum = 0;
         int currentFileBlock = fileBlocks[0];
@@ -36,7 +33,6 @@ public class Day9 : BaseDay
             if (currentFileBlock == fileBlocks[currentFileBlockIndex])
             {
                 currentFileBlockIndex++;
-                compressedSpace.Add(currentFileBlock);
                 sum += compressedSpaceIndex * currentFileBlock;
             }
             else
@@ -44,7 +40,6 @@ public class Day9 : BaseDay
                 currentFileBlock = fileBlocks[currentFileBlockIndex];
                 for (int freeBlock = 0; freeBlock < freeBlocks[freeBlockIndex]; freeBlock++)
                 {
-                    compressedSpace.Add(fileBlocks[fileBlocks.Length - indexFromBack - 1]);
                     sum += compressedSpaceIndex * fileBlocks[fileBlocks.Length - indexFromBack - 1];
                     indexFromBack++;
                     compressedSpaceIndex++;
@@ -53,6 +48,84 @@ public class Day9 : BaseDay
                 compressedSpaceIndex--;
             }
         }
+
+        return sum.ToString();
+    }
+
+    private record FileBlock(int Size, int FileId)
+    {
+        public bool Processed { get; set; } = false;
+        public bool Moved { get; set; } = false;
+
+    }
+
+    private record FreeBlock(int Size, List<FileBlock> FileBlocks)
+    {
+        public int FreeSpace => Size - FileBlocks.Sum(x => x.Size);
+    }
+
+    public override string Solution2()
+    {
+        var fileBlocks = Input[0].Where((_, i) => i % 2 == 0).Select((c, i) => new FileBlock(int.Parse(c.ToString()), i)).Reverse().ToArray();
+        var freeBlocks = Input[0].Where((_, i) => i % 2 != 0).Select(c => new FreeBlock(int.Parse(c.ToString()), new())).ToArray();
+
+        foreach (var freeBlock in freeBlocks)
+        {
+            foreach (var fileBlock in fileBlocks.Where(fileBlock => !fileBlock.Processed))
+            {
+                if (freeBlock.FreeSpace == 0) break;
+
+                if (fileBlock.Size <= freeBlock.FreeSpace)
+                {
+                    freeBlock.FileBlocks.Add(fileBlock);
+                    fileBlock.Processed = true;
+                }
+            }
+        }
+
+        foreach (var fileBlock in fileBlocks)
+        {
+            if (freeBlocks.Any(freeBlock => freeBlock.FileBlocks.Contains(fileBlock)))
+            {
+                fileBlock.Moved = true;
+            }
+        }
+
+        long sum = 0;
+        //var newFileId = 0;
+        //var index = 0;
+        //foreach (var fileBlock in fileBlocks.Reverse().ToArray())
+        //{
+        //    if (!fileBlock.Moved)
+        //    {
+        //        for (int fileBlockIndex = 0; fileBlockIndex < fileBlock.Size; fileBlockIndex++)
+        //        {
+        //            sum += fileBlock.FileId * newFileId++;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        newFileId += fileBlock.Size;
+        //    }
+
+        //    if (freeBlocks.Length == index) break;
+
+        //    var freeBlock = freeBlocks[index];
+        //    foreach (var movedFileBlock in freeBlock.FileBlocks)
+        //    {
+        //        for (int fileBlockIndex = 0; fileBlockIndex < movedFileBlock.Size; fileBlockIndex++)
+        //        {
+        //            sum += movedFileBlock.FileId * newFileId++;
+        //        }
+        //    }
+        //    newFileId += freeBlock.FreeSpace;
+
+        //    index++;
+        //}
+
+
+
+
 
         return sum.ToString();
     }
